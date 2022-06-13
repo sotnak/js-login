ec162f536bb8e2f154d979aff4818ac241f52abd
antospa2 <antospa2@cvut.cz>
2022-06-13 17:03:09 +0200

    updated dbsetup

--- scripts/dbsetup.js
+++ scripts/dbsetup.js
@@ -4,6 +4,13 @@ const {environment} = require("../environment")
 const url = "mongodb://"+environment.mongo_addr
 const client = new MongoClient(url);
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 async function run() {
     const db = client.db('login')
 
@@ -26,14 +33,19 @@ async function run() {
             await db.dropCollection(coll)
         }
 
-        await db.createCollection(coll)
+        console.log(await db.createCollection(coll))
     }
 
-    await db.collection('users').createIndex({username:1})
-    await db.collection('tokens').createIndex({username:1})
-    await db.collection('revokedTokens').createIndex({token:1})
-    await db.collection('nonce').createIndex({nonce:1})
+    console.log(await db.collection('users').createIndex({username:1}))
+    console.log(await db.collection('tokens').createIndex({username:1}))
+    console.log(await db.collection('revokedTokens').createIndex({token:1}))
+    console.log(await db.collection('nonce').createIndex({nonce:1}))
 
     client.close()
-  }
-  run()
+}
+  
+rl.question('Are you sure? All data will be erased! Type \'yes\' to continue.\n', (answer) => {
+    if(answer == 'yes')
+        run()
+    rl.close();
+});
